a4cfdb201f11043ddc5c902dfae327e811e5ec6c
saineelagiri <48248640+saineelagiri@users.noreply.github.com>
2025-07-05 13:45:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+
+
+access_key = "AKIA356SJ6QEGA6QPXFA"
+
+secret_key = "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"
+
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+
+ami = "ami-0f3f13f145e66a0a3"
+
+instance_type = "t2.micro"
+
+tags = {
+
+Name = "terrafrom ec2"
+
+}
+
+}
